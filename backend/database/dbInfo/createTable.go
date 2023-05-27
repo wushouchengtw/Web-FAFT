3ef5ccf5157b33eea013a132bf6ae32bfc11fe58
KevinWu <wushouchengtw@gmail.com>
2023-05-27 12:03:39 +0800

    Add GenerateTableMap and CreateTable
    Add generate to parse the db table struct and create a table
    automatically.

--- backend/database/dbInfo/createTable.go
+++ backend/database/dbInfo/createTable.go
@@ -2,8 +2,57 @@ package dbInfo
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 )
+
+type KeyType struct {
+	Key  string `json:"key"`
+	Type string `json:"type"`
+}
+
+type TableConfig struct {
+	TableName tableName `json:"tablename"`
+	Sql       []KeyType `json:"sql"`
+}
+
+type tableName string
+
+type tableJson struct {
+	jsonFile string
+}
+
+func (s *tableJson) GenerateTableMap() map[tableName]string {
+	out, _ := ioutil.ReadFile(s.jsonFile)
+	var t []TableConfig
+	if err := json.Unmarshal(out, &t); err != nil {
+		log.Fatalf("Failed to unmarshal: %s", out)
+	}
+
+	tableMap := map[tableName]string{}
+	for _, table := range t {
+		var sqlStmt string
+		for index, keyMap := range table.Sql {
+			if index == len(table.Sql)-1 {
+				sqlStmt += keyMap.Key + " " + keyMap.Type
+			} else {
+				sqlStmt += keyMap.Key + " " + keyMap.Type + ","
+			}
+		}
+		tableMap[table.TableName] = sqlStmt
+	}
+	return tableMap
+}
+
+func CreateTable(DB *sql.DB, tableName, keyMap string) error {
+	sql := `CREATE TABLE IF NOT EXISTS %s(`
+	sql += keyMap + `); `
+
+	sqlStmt := fmt.Sprintf(sql, tableName)
+	return RunSqlStmt(DB, sqlStmt)
+}
 
 func CreateTableCaseTable(DB *sql.DB) error {
 	sql := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
